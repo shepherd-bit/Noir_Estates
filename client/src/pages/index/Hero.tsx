44a0a886ef3e2e0f415795f9a06440f2ebbc2936
Shepherd-bit <titusaoluoch@gmail.com>
2026-09-20 17:09:40 +0300

    Update Hero.tsx

--- client/src/pages/index/Hero.tsx
+++ client/src/pages/index/Hero.tsx
@@ -30,23 +30,6 @@ export default function Hero() {
             <div className="flex -space-x-2 overflow-hidden">
               {/* Profile Image Containers */}
               <div className="inline-block h-9 w-9 rounded-full ring-2 ring-[#FBF9F5] bg-gray-200 overflow-hidden">
-<<<<<<< Updated upstream
-                <img src="./Hero-Avatars/Avatar-4.png" alt="Collector 1" className="h-full w-full object-cover" />
-              </div>
-              <div className="inline-block h-9 w-9 rounded-full ring-2 ring-[#FBF9F5] bg-gray-200 overflow-hidden">
-                <img src="./Hero-Avatars/Avatar-11.png" alt="Collector 2" className="h-full w-full object-cover" />
-              </div>
-              <div className="inline-block h-9 w-9 rounded-full ring-2 ring-[#FBF9F5] bg-gray-200 overflow-hidden">
-                <img src="./Hero-Avatars/Avatar-16.png" alt="Collector 3" className="h-full w-full object-cover" />
-=======
-                <img src="./Hero-Avatars/Avatar-1.png" alt="Collector 1" className="h-full w-full object-cover" />
-              </div>
-              <div className="inline-block h-9 w-9 rounded-full ring-2 ring-[#FBF9F5] bg-gray-200 overflow-hidden">
-                <img src="./Hero-Avatars/Avatar-2.png" alt="Collector 2" className="h-full w-full object-cover" />
-              </div>
-              <div className="inline-block h-9 w-9 rounded-full ring-2 ring-[#FBF9F5] bg-gray-200 overflow-hidden">
-                <img src="./Hero-Avatars/Avatar-3.png" alt="Collector 3" className="h-full w-full object-cover" />
->>>>>>> Stashed changes
               </div>
             </div>
             <div className="text-xs text-gray-600">
